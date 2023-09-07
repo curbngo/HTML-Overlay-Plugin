8652e191352173d32182ab06c4872101d4a6e31b
hirshy <hirshy@gmail.com>
2023-09-07 18:16:19 -0400

    Change function name back to 'show'

--- www/CordovaHTMLOverlay.js
+++ www/CordovaHTMLOverlay.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 var CordovaHTMLOverlay = {
-    showOnLoad: function (htmlStr, successCallback, errorCallback) {
+    show: function (htmlStr, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, 'CordovaHTMLOverlay', 'show', [htmlStr]);
     },
     hide: function (successCallback, errorCallback) {
